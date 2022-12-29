0fc73d119e0fefef90c2b9225190be5ee3055bb0
Zomdao <107764999+Zomdao@users.noreply.github.com>
2022-12-30 00:31:11 +0100

    Update Config.java

--- Shared/src/main/java/dev/tr7zw/itemswapper/config/Config.java
+++ Shared/src/main/java/dev/tr7zw/itemswapper/config/Config.java
@@ -5,7 +5,6 @@ public class Config {
     public int configVersion = 1;
     public boolean toggleMode = false;
     public boolean showCursor = false;
-    public boolean serverPreventModUsage = false;
     public boolean editMode = false;
     public boolean creativeCheatMode = true;
     public boolean ignoreHotbar = true;
